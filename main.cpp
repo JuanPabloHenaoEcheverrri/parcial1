4e432dd352eef2f9c54937eacc9f14a4af7ae4e0
Juan Pablo Henao <juan.henaoe@udea.edu.co>
2020-10-17 17:07:19 -0500

    se ajustan variables, se encuentarn errores en el analicis, por lo que se cambian ciertas condiciones

--- main.cpp
+++ main.cpp
@@ -7,20 +7,25 @@ int main()
 {
     float xo, yo, vxo, vyo, xoi, yoi;
     float xd, yd, vxd, vyd, xdi, ydi;
-    int anguloo, angulod, t=0, d,vo, vd ;
+    int anguloo, angulod, t=0, d,vo, vd, g=9.8, t1=0 ;
     return 0;
 
     while(xo != xd || yo!=yd){
-        vxo=vo*sin(anguloo*t);
+        vxo=vo*cos(anguloo*t);
         xo=xoi+vxo*t;
+        vyo=vo*sin(anguloo*t);
+        yo=yoi+vyo*t-(1/2)*g*pow(t,2);
+
         t++;
-         if (t>=2){
+
              if(sqrt(pow(xo,2)+pow(yo,2))-sqrt(pow(xdi,2)+pow(ydi,2))<=0.025*d){
+                 angulod=atan(yo/xo);
+
 
              }
 
 
-         }
+
 
 
     }
