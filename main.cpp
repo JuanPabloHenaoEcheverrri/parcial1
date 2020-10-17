387e7e441b61f06e3fe40a5e607d115d960fb116
Juan Pablo Henao <juan.henaoe@udea.edu.co>
2020-10-17 16:25:36 -0500

    se empieza la codificación cin las variables implementadas, y el analisis del problema;
    Signed-off-by: Juan Pablo Henao <juan.henaoe@udea.edu.co>

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    float xo, yo, vxo, vyo, xoi, yoi;
+    float xd, yd, vxd, vyd, xdi, ydi;
+    int anguloo, angulod, t=0, d,vo, vd ;
+    return 0;
+
+    while(xo != xd || yo!=yd){
+        vxo=vo*sin(anguloo*t);
+        xo=xoi+vxo*t;
+        t++;
+         if (t>=2){
+             if(xo==)
+
+             xd=xdi;
+         }
+
+
+    }
+}
