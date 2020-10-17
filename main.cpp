6f18edbba4109dd0df87396c76280082f5efcfd3
Juan Pablo Henao <juan.henaoe@udea.edu.co>
2020-10-17 17:37:40 -0500

    lo que se avanzó del proyecto, hasta el tiempo dado

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
 {
     float xo, yo, vxo, vyo, xoi, yoi;
     float xd, yd, vxd, vyd, xdi, ydi;
-    int anguloo, angulod, t=0, d,vo, vd, g=9.8, t1=0 ;
+    int anguloo, angulod, t=0, d,vo, vd, g=9.81, t1=0 ;
     return 0;
 
     while(xo != xd || yo!=yd){
@@ -20,7 +20,11 @@ int main()
 
              if(sqrt(pow(xo,2)+pow(yo,2))-sqrt(pow(xdi,2)+pow(ydi,2))<=0.025*d){
                  angulod=atan(yo/xo);
-                 t++;
+                 vxo=vo*cos(anguloo*t);
+                 xo=xoi+vxo*t;
+                 vyo=vo*sin(anguloo*t);
+                 yo=yoi+vyo*t-(1/2)*g*pow(t,2);
+                 t1++;
 
 
              }
